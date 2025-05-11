072b055ccc3880226c2bb8d66d8b26aadaab09c1
carlosfr46 <cr46@njit.edu>
2025-05-11 00:07:27 -0400

    DATA parsed

--- src/App.js
+++ src/App.js
@@ -14,14 +14,41 @@ function App() {
 
   useEffect(() => {
     d3.csv(process.env.PUBLIC_URL + "/heart_attack_prediction_indonesia.csv", d => ({
-      sleep_hours: +d.sleep_hours,
-      stress_level: +d.stress_level,
       age: +d.age,
-      [selectedColumn]: d[selectedColumn],
-    })).then(setData);
-  }, [selectedColumn]);
-
+      physical_activity: +d.physical_activity,
+      stress_level: +d.stress_level,
+      smoking_status: (() => {
+        if (d.smoking_status === "Never") return 0;
+        if (d.smoking_status === "Former") return 1;
+        if (d.smoking_status === "Current") return 2;
+        return null;
+      })(),
+      alcohol_consumption: +d.alcohol_consumption,
+      sleep_hours: +d.sleep_hours,
+      air_pollution: +d.air_pollution_exposure,
+      income_level: (() => {
+        if (d.income_level === "Low") return 0;
+        if (d.income_level === "Middle") return 1;
+        if (d.income_level === "High") return 2;
+        return null;
+      })(),
+      family_history: +d.family_history,
+      dietary_habits: (() => {
+        if (d.dietary_habits === "Unhealthy") return 0;
+        if (d.dietary_habits === "Average") return 1;
+        if (d.dietary_habits === "Healthy") return 2;
+        return null;
+      })(),
+      obesity: +d.obesity,
+      cholesterol_level: +d.cholesterol_level,
+    })).then(parsed =>{
+      console.log("Parsed Data:", parsed);
+      setData(parsed);
+    });
+  }, []);
+  
   const filteredData = data.filter(d => d.age <= age);
+
 
   return (
     <div className="dashboard-container">
